d06d168531cdbdd4ab079ce901ab33d3ad1ba140
floh0 <florian.dasilva@ensiie.fr>
2020-12-10 00:25:17 +0100

    day9 (part2)

--- day9.py
+++ day9.py
@@ -16,3 +16,16 @@ while part1 < len(i) and check(part1):
 num = i[part1]
 print(num, part1)
 
+s = i[:part1]
+
+def continuous(input, size):
+	for x in range(len(input)-size+1):
+		if sum(input[x:x+size]) == num:
+			return input[x:x+size]
+
+for x in range(part1,1,-1):
+	part2 = continuous(s, x)
+	if part2:
+		print(part2)
+		print(min(part2),max(part2))
+		print(min(part2)+max(part2))
